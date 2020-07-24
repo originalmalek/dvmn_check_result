f1916c7ccff5b77af49cbab549977f95a9e24e4d
originalmalek <56593369+originalmalek@users.noreply.github.com>
2020-07-24 11:38:47 +0500

    Fixed errors after code review

--- main.py
+++ main.py
@@ -4,45 +4,52 @@ import telebot
 from dotenv import load_dotenv
 from time import sleep
 
-def send_message(new_attempts):
-    bot = telebot.TeleBot(token_tlgrm, parse_mode='MARKDOWN')
+def send_message(tg_token, tg_chat_id, new_attempts):
+    bot = telebot.TeleBot(tg_token, parse_mode='MARKDOWN')
 
     for attempt in new_attempts:
         lesson_title = attempt['lesson_title']
-        if attempt['is_negative'] == True:
+        if attempt['is_negative']:
             is_negative = 'Преподаватель *не принял* вашу работу. Нужно исправить ошибки'
         else:
             is_negative = 'Преподаватель *принял* вашу работу! Можете приступать к следующему уроку'
 
-        bot.send_message(70148681, f'{lesson_title}\n*Работа проверена!*\n\n{is_negative}')
+        bot.send_message(tg_chat_id, f'{lesson_title}\n*Работа проверена!*\n\n{is_negative}')
 
 
 
-def check_info(token_dvmn):
-    url = 'https://dvmn.org/api/long_polling/'
-    header = {'Authorization': f'Token {token_dvmn}'}
+def check_dvmn_result(dvmn_token, tg_token, tg_chat_id):
+    header = {'Authorization': f'Token {dvmn_token}'}
+    payload = {}
 
     while True:
+        url = 'https://dvmn.org/api/long_polling/'
         try:
-            response = requests.get(url, headers=header).json()
-            if 'timestamp_to_request' in response:
-                timestamp = str(response['timestamp_to_request'])
-            if 'last_attempt_timestamp' in response:
-                timestamp = str(response['last_attempt_timestamp'])
-                send_message(response['new_attempts'])
-            url = f'https://dvmn.org/api/long_polling/?timestamp={timestamp}'
+            response = requests.get(url, headers=header, params=payload, timeout=60)
+            if response.ok:
+                response = response.json()
+                if 'timestamp_to_request' in response:
+                    timestamp = str(response['timestamp_to_request'])
+
+                if 'last_attempt_timestamp' in response:
+                    timestamp = str(response['last_attempt_timestamp'])
+                    send_message(tg_token, tg_chat_id, response['new_attempts'])
+
+                payload = {'timestamp': timestamp}
         except requests.exceptions.ReadTimeout:
-            sleep(60)
+            pass
         except requests.exceptions.ConnectionError:
             sleep(60)
 
 
 def main():
     load_dotenv()
-    token_dvmn = os.getenv('TOKEN_DVMN')
-    global token_tlgrm
-    token_tlgrm = os.getenv('TOKEN_TLGRM')
-    check_info(token_dvmn)
+
+    dvmn_token = os.getenv('DVMN_TOKEN')
+    tg_token = os.getenv('TG_TOKEN')
+    tg_chat_id = os.getenv('TG_CHAT_ID')
+
+    check_dvmn_result(dvmn_token, tg_token, tg_chat_id)
 
 
 if __name__ == '__main__':
